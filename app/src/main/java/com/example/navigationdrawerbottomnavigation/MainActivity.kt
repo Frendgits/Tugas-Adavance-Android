0b2affb26efe3bc8e69e3534ee1e36ac2b3cc180
Frendy Siswanto <swntfrendy@gmail.com>
2023-12-10 22:51:45 +0700

    first commit

---
+++ app/src/main/java/com/example/navigationdrawerbottomnavigation/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.navigationdrawerbottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.navigationdrawerbottomnavigation.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val toogle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener{ item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_person -> openFragment(PersonFragment())
+            }
+            true
+        }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_list -> openFragment(HomeFragment())
+            R.id.nav_list -> openFragment(PersonFragment())
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressedDispatcher.onBackPressed()
+        }
+    }
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+}
